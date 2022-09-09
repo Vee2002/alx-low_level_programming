1afbdbf07bd12fedb58d92d915106260eba73dbf
Vee2002 <jackyvee20@gmail.com>
2022-09-09 09:48:57 -0700

    Pritn alphabet

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description- Prints the alphabets in lowercase
+ *
+ * Return -Always 0(Success)
+ */
+int main(void)
+{
+	char ch = 'a';
+	while(a < z)
+	{
+		putchar(ch);
+		ch++
+	}
+
+return (0);
--- 0x01-variables_if_else_while/README.md
+++ 0x01-variables_if_else_while/README.md
@@ -1,1 +1,1 @@
-Last digit
+Print Alphabets

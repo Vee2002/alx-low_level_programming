9883eaf0ffb2bc3ed5db2d8d567c58c2590d98ff
Vee2002 <jackyvee20@gmail.com>
2022-09-10 14:06:14 -0700

    Lower and Upper Alphabet

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -3,19 +3,20 @@
 /**
  * main - Entry point
  *
- * Description- Prints the alphabets in lowercase and uppercase
+ * Desription: Prints the alphabets in lowercase,and then in uppercase followed by a new line
  *
- * Return -Always 0(Success)
+ * Return: Always 0(Success)
  */
 int main(void)
 {
 	char ch;
 	char CH;
-	for(ch = 'a'; ch <= 'z'; ch++)
+
+	for (ch = 'a'; ch <= 'z'; ch++)
 	{
 		putchar(ch);
 	}
-	for(CH = 'A'; CH <= 'Z'; CH++)
+	for (CH = 'A'; CH <= 'Z'; CH++)
 	{
 		putchar(CH);
 	}

e36259a6739c40d98b711f76dc568ce5ffea6c09
Vee2002 <jackyvee20@gmail.com>
2022-09-14 13:07:47 -0700

    10-add.c

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -2,7 +2,7 @@
 /**
  * main -Entry point
  * print_alphabet: entry of function
- * function add - adds two numbers together
+ * function add - Function adds the two numbers together
  * @x: First number to be added
  * @y: Second number to be added
  * Return: the sum of two numbers

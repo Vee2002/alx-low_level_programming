ecbed23076517f02603c50d9e34e6bab96a91fc2
Vee2002 <jackyvee20@gmail.com>
2022-09-14 14:31:30 -0700

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -3,11 +3,11 @@
  */
 
 #include "main.h"
-/** print_sign: protoype
+/** print_sign: Checks if a number is positive or negative
  * @n: Parameter passed to function
  * Description: Checks if a number is positive or negative
  * Return: 1 if positive and 0 if zero
- * -1 if negative, '/' if not a digit
+ *              -1 if negative, '/' if not a digit
  */
 int print_sign(int n)
 {

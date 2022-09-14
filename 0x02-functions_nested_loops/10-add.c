fb6158a509a6868c4d1aa7c837b16fe9a59dba78
Vee2002 <jackyvee20@gmail.com>
2022-09-14 13:09:41 -0700

    10-add.c

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,8 +1,6 @@
 #include "main.h"
 /**
- * main -Entry point
- * print_alphabet: entry of function
- * function add - Function adds the two numbers together
+ * add - Function adds the two numbers together
  * @x: First number to be added
  * @y: Second number to be added
  * Return: the sum of two numbers

4e98b8575d7c78c32f6ca9eab6914d08d7ba6d8b
Vee2002 <jackyvee20@gmail.com>
2022-09-14 06:22:53 -0700

    8-24_hours.c

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * main - Entry point 
+ * description: prints every minute of the day of Jack Bauer,
+ *  starting from 00:00 to 23:59
+ *  Return: Always 0(Success)
+ */
+void jack_bauer(time)
+{
+	
+	for (time = 00:00; time <= 23:59; time++)
+	{
+		jack_bauer(time);
+	}
+}

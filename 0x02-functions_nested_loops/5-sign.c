0199b1fbd751fd568260baec5029fd5fe33d723d
Vee2002 <jackyvee20@gmail.com>
2022-09-14 05:16:05 -0700

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * main Entry point 
+ * main Entry point
  * print_sign - This program determines if
  * a number is positive or negative or zero
  * Return: 1 if integer is positive and 0 if zero
@@ -17,7 +17,7 @@ int print_sign(int n)
 		_putchar('0');
 		return (0);
 	}
-	else if(n < 0)
+	else if (n < 0)
 	{
 		_putchar('-');
 		return (-1);

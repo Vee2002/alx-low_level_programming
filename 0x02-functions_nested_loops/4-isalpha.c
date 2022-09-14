c75356d560abb7fcc3e7517d0c153c493167fb1b
Vee2002 <jackyvee20@gmail.com>
2022-09-14 13:44:34 -0700

    4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,9 +1,13 @@
+/*
+ * File: 4-isalpha.c
+ * Function - Entry
+ */
 #include "main.h"
 /**
  * _isalpha: Function that checks alphabetic order
  * @c: Parameter of the function
- * Return: 1 if character 
- * return 0 otherwise
+ * Return: 1 if character
+ * * return 0 otherwise
  */
 int _isalpha(int c)
 /*This is the function prototype and c is the parameter*/

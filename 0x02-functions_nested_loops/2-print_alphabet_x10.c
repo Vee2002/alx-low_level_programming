2a829fe108e9136de0ffdb6255a2920154d4cf27
Vee2002 <jackyvee20@gmail.com>
2022-09-14 07:04:05 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,5 +1,6 @@
 #include "main.h"
 /**
+ * File: 2-print_alphabet_x10.c
  * main - Entry point
  * print_alphabet_x10 - This code writes a function
  * that prints 10 times the alphabets in lowecase

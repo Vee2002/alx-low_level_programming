d8c088cef6dd9da1e4404ca7ffb8aef31244fccf
Vee2002 <jackyvee20@gmail.com>
2022-09-15 08:25:00 -0700

    0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -5,17 +5,16 @@
 *             checks for
 *             uppercase letters
 * @c: Parameter passed to funcrion 
-* Return: 1 if characters are uppercase,
-* and 0 if characters are otherwise
+* Return: 1 if uppercase,0 if otherwise 
 */
 int _isupper(int c)
 {
-if (c == 'A' && c <= 'Z')
-{
-return (1);
-}
-else
-{
-return (0);
-}
+	if (c == 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0)'
+	}
 }

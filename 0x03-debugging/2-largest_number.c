8e569555277e20b60bb5d31e3de75fc6a5c62574
Vee2002 <jackyvee20@gmail.com>
2022-09-15 04:16:09 -0700

    2-largest_number.c

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -1,21 +1,23 @@
 #include "main.h"
-/**                                                                                                                       
- * largest_number - function returns the largest of 3 numbers                                                                  
- * @a: first parameter passed                                                                                                   
- * @b: second parameter passed                                                                                                  
- * @c: parameter passed                                                                                                         
- * Return: largest number                                                                                                       
- */                                                                                                                             
-int largest_number(int a, int b, int c)                                                                                         
-{                                                                                                                               
-if (a > b && a > c)                                                                                                             {                                                                                                                               return (a);                                                                                                                     
+/**                                                                                                         
+* largest_number - function returns the largest of 3 numbers
+* @a: first parameter passed
+* b: second parameter passed
+* @c: parameter passed
+* Return: largest
+*/                                                                                                                          
+int largest_number(int a, int b, int c)
+{
+if(a > b && a > c)
+{
+return (a);
 } 
-else if (b > a && b > c)                                                                                                        
-{                                                                                                                             
-return (b);                                                                                                                     
-}                                                                                                                            
+else if (b > a && b > c)
+{
+return (b);
+}
 else
-{                                                                                                                               
+{                                                                                                                      
 return (c);
 }
 }               

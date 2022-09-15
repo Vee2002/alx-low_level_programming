2269b770cce30117133bec5f881b24e7b038afdd
Vee2002 <jackyvee20@gmail.com>
2022-09-15 04:21:53 -0700

    2-largest_number.c

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -1,11 +1,11 @@
 #include "main.h"
-/**                                                                                                         
+/**
 * largest_number - function returns the largest of 3 numbers
 * @a: first parameter passed
-* b: second parameter passed
+* @b: second parameter passed
 * @c: parameter passed
 * Return: largest
-*/                                                                                                                          
+*/
 int largest_number(int a, int b, int c)
 {
 if(a > b && a > c)
@@ -17,7 +17,7 @@ else if (b > a && b > c)
 return (b);
 }
 else
-{                                                                                                                      
+{
 return (c);
 }
-}               
+}          

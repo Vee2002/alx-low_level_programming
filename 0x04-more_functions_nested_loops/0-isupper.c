5b4376a4ba15752910c3c7f4b2aeb310686485c8
Vee2002 <jackyvee20@gmail.com>
2022-09-15 08:02:28 -0700

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* main - Check the code
+* 
+* Return: Always 0(Success)
+*/
+int _isupper(int c)
+{
+if (c = 'A'; c <= 'Z'; c++)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}

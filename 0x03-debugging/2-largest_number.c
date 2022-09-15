7145e98fc0b76eecdf8057c7806c4d8fd3642165
Vee2002 <jackyvee20@gmail.com>
2022-09-15 03:58:30 -0700

    2-largest_number.c

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -1,23 +1,45 @@
-/**
- * largest_number - function returns the largest of 3 numbers
- * @a: first parameter passed
- * @b: second parameter passed
- * @c: parameter passed
- * Return: largest number
- */
-int largest_number(int a, int b, int c)
-{
+/**                                                                                                                             
 
-if (a > b && a > c)
-{
-return (a);
-}
-else if (b > a && b > c)
-{
-return (b);
-}
-else
-{
-return (c);
-}
-}
+ * largest_number - function returns the largest of 3 numbers                                                                   
+
+ * @a: first parameter passed                                                                                                   
+
+ * @b: second parameter passed                                                                                                  
+
+ * @c: parameter passed                                                                                                         
+
+ * Return: largest number                                                                                                       
+
+ */                                                                                                                             
+
+int largest_number(int a, int b, int c)                                                                                         
+
+{                                                                                                                               
+
+                                                                                                                                
+
+if (a > b && a > c)                                                                                                             
+
+{                                                                                                                               
+
+return (a);                                                                                                                     
+
+}                                                                                                                               
+
+else if (b > a && b > c)                                                                                                        
+
+{                                                                                                                               
+
+return (b);                                                                                                                     
+
+}                                                                                                                               
+
+else                                                                                                                            
+
+{                                                                                                                               
+
+return (c);                                                                                                                     
+
+}                                                                                                                               
+
+}               

dc400e0560120b6121a96b452b4ae9c299018f89
Vee2002 <jackyvee20@gmail.com>
2022-09-16 04:24:22 -0700

    1-isdigit.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,8 +1,7 @@
 #include "main.h"
 
 /**
-* _isupper: Function to check for uppercase characters and
-* return 0 or 1
+* _isupper: Function to check for uppercase characters 
 * @c : Parameter passed to function _isupper
 * Return: 1 if uppercase, 0 if otherwise
 */

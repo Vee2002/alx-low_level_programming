87a14b083d5771b9e51b5909e0e8bf90c8ae775d
Vee2002 <jackyvee20@gmail.com>
2022-09-16 04:37:13 -0700

    int mul(int a, int b);

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * mul - Function to multiply the integers
+ * @a: First parameter passed
+ * @b: Second parameter passed
+ * Return: Alwats 0
+ */
+int mul(int a, int b)
+{
+	return(a * b);
+}

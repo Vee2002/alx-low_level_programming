73d0f73b1f13fee26a5273007004eba6e9364caa
Vee2002 <jackyvee20@gmail.com>
2022-09-16 15:09:41 -0700

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -9,7 +9,7 @@ int main(void)
 {
 	int i = 1;
 
-	while  (++i < 100)
+	while  (i++ < 100)
 	{
 		if ((i % 3 == 0) && (i % 5 == 0))
 		{
@@ -27,7 +27,7 @@ int main(void)
 			}
 			else
 			{
-				printf("Buzz ");
+				printf("Buzz");
 			}
 		}
 	}

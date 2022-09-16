307332bf26d9e5d89e909779bb19c36fd9f5a7e0
Vee2002 <jackyvee20@gmail.com>
2022-09-16 15:17:36 -0700

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -21,14 +21,7 @@ int main(void)
 		}
 		else if ((i % 5) == 0)
 		{
-			if (i != 100)
-			{
-				printf("Buzz ");
-			}
-			else
-			{
-				printf("Buzz ");
-			}
+			printf("Buzz ");
 		}
 	}
 return (0);

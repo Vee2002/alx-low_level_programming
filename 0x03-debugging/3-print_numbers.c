b9fbd5b0dbf326d0ea35d4f452b73efc54ff0eef
Vee2002 <jackyvee20@gmail.com>
2022-09-16 14:48:18 -0700

    3-print_numbers.c

--- 0x03-debugging/3-print_numbers.c
+++ 0x03-debugging/3-print_numbers.c
@@ -9,11 +9,11 @@
 */
 void print_numbers(void)
 {
-int n;
+	int n;
 
-for (n = 48; n < 58; n++)
-{
-putchar(n);
-}
-putchar('\n');
+	for (n = 48; n < 58; n++)
+	{
+	_putchar(n);
+	}
+_putchar('\n');
 }

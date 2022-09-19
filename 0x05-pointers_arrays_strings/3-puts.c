2dadba46bf83266342ff5810c20e1176b3ea4275
Vee2002 <jackyvee20@gmail.com>
2022-09-19 10:11:32 -0700

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,14 @@
+#include  "main.h"
+
+/**
+*_puts - Prints a string
+*@str: prints a string
+*Return: returns nothing
+*/
+
+void _puts(char *str)
+{
+char s[] = "This is my code";
+putchar(s);
+putchar('\n');
+}

b6a2e9c734d198c02111f46e33365ce6c8037bbd
Vee2002 <jackyvee20@gmail.com>
2022-09-20 08:19:05 -0700

    6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
 *puts2 - prints half a string
-*@str - Parameter passed
+*@str: Parameter passed
 *Return: returns nothing
 */
 
@@ -10,11 +10,8 @@ void puts2(char *str)
 int string;
 
 for (string = 0; str[string] != '\0'; string++)
-{
 if (string % 2 == 0)
-{
-putchar(str[string]);
-}
-putchar('\n');
-}
+
+_putchar(str[string]);
+_putchar('\n');
 }

fd4dadeb9630bd770fcb7b3a3351d2138cbb10f3
Vee2002 <jackyvee20@gmail.com>
2022-09-20 01:50:08 -0700

    6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+/**
+*puts2 - Prints every character of a string
+*@str: Parameter passed
+*Return: returns void
+*/
+
+void puts2(char *str)
+{
+int i = 0;
+while (*(str + i) != '\0')
+{
+	if (i % 2 == 0)
+	{
+		putchar(*(str + i));
+	}
+	else 
+	{
+		putchar('\n');
+	}
+}

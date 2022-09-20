b69a8bd06d6c8922deb859ec71155baeebbcd0ab
Vee2002 <jackyvee20@gmail.com>
2022-09-20 09:13:45 -0700

    9-strcpy.c

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -3,11 +3,13 @@
 *_strcpy - Copies the string
 *@dest: First parameter passed
 *@src: Second parameter passed
-*Return: returns nothing
+*Return: Always 0 (Success)
 */
 
 char *_strcpy(char *dest, char *src)
 {
+int a;
+
 for (a = 0; src[a] != '\0'; a++)
 dest[a] = src[a];
 dest[a] = '\0';

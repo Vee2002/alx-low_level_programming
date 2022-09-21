43703cfb06895d587ab817a43a50c83945dbea62
Vee2002 <jackyvee20@gmail.com>
2022-09-21 12:42:58 -0700

    1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -4,12 +4,13 @@
  * _strncat - Concatenates strings
  * @dest: First parameter
  * @src: Second parameter
+ * @n: Third parameter
  * Return: returns character
  */
 
 char *_strncat(char *dest, char *src, int n)
 {
-strncat(dest,src,n);
+strncat(dest, src, n);
 
 return (dest);
 }

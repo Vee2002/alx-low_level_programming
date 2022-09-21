ef37379005cf7807736680154adaacc61468c8ed
Vee2002 <jackyvee20@gmail.com>
2022-09-21 12:38:14 -0700

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -11,6 +11,7 @@
 char *_strcat(char *dest, char *src)
 {
 int len = 0;
+int i;
 
 while (dest[len])
 {
@@ -19,6 +20,7 @@ len++;
 for (i = 0; src[i] != '\0'; i++)
 {
 dest[len] = src[i];
+len += 1;
 }
 dest[len] = '\0';
 

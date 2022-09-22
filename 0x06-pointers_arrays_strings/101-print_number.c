743aff2e0b6d30420c11a4fb413d835a33579afa
Vee2002 <jackyvee20@gmail.com>
2022-09-22 13:02:53 -0700

    101-print_number.c

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+*print_number - Prints an integer
+*@n: Parameter passed
+*Return: returns nothing
+*/
+
+void print_number(int n)
+{
+unsigned int num = n;
+
+if (n < 0)
+{
+	putchar('-');
+n = -n;
+}
+if (n / 10)
+{
+print_numbers(n / 10);
+}
+_putchar((n % 10) + '0');
+}

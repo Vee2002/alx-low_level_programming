582e04e32f86b869c9717885c695c7b195f7bf36
Vee2002 <jackyvee20@gmail.com>
2022-09-23 13:11:51 -0700

    7-leet.c

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * leet - Function for encoding
+ * x: Parameter passed
+*Return: returns a character
+*/
+
+char *leet(char *x)
+{                                                                                                                               
+int a = 0, b, l = 5;
+char tr[5] = {'A', 'E', 'O', 'T', 'L'};
+Char trc[5] = {'4', '3', '0', '7', '1'};
+while x[a]
+{
+b = 0;
+while
+{
+if (x[a] == tr[b] || x[a] - 32 == tr[b])
+{
+	x[a] = trc[b];
+b++;
+}
+a++;
+}
+}
+return (x);
+}

e41d020eaadf900c060b0618ddd10442de7805e5
Vee2002 <jackyvee20@gmail.com>
2022-09-26 06:35:26 -0700

    1-memcpy.c

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -3,13 +3,13 @@
 
 /**
 *_memcpy - Function that copies a block of memory
-*dest: First parameter passed
-*src: Second parameter passed
-*n: Third parameter passed
+*@dest: First parameter passed
+*@src: Second parameter passed
+*@n: Third parameter passed
 *Return: returns a character
 */
 
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-memcpy(dest,src,strlen(src)+1);
+memcpy(dest,src,n);
 }

b971111e365301c49d443ebd4f6737da0986ffed
Vee2002 <jackyvee20@gmail.com>
2022-09-26 10:51:31 -0700

    100-set_string.c

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * set_string - Sets pointer to string
+ * @s: First parameter
+ * @to: Second parameter
+ * Return: returns nothing
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}

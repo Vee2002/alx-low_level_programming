0720036ca1e58003ec9f8a8d1a7a8c2a02be0288
Vee2002 <jackyvee20@gmail.com>
2022-09-26 06:52:48 -0700

    0-memset.c

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,23 @@
+#include <string.h>
+#include "main.h"
+
+/**
+*_memset - Function that fills
+*memory with a particular value
+*@s: First parameter passed
+*@b: Second parameter passed
+*@n: Third parameter passed
+*Return: returns a character
+*/
+
+char *_memset(char *s, char b, unsigned int n)
+{
+unsigned int i;
+
+for (i = o; i < n; i++)
+{
+s[i] = b[i]
+}
+
+return (s);
+}

e5d9e9340e0a16ecb1aa10646398ed19f96c8cb0
Vee2002 <jackyvee20@gmail.com>
2022-09-26 10:26:14 -0700

    7-print_chessboard.c

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,22 @@
+#include "main."
+
+/**
+*print_chessboard - Prints a chessbord
+*@a: First parameter passed
+*Return: returns nothing
+*/
+
+void print_chessboard(char (*a)[8])
+{
+int i, j;
+
+for (i = 0; i < 8; i++)
+{
+for (j = 0; j < 8; j++)
+{
+_putchar([a][i][j]);
+}
+_putchar('\n');
+}
+return (0);
+}

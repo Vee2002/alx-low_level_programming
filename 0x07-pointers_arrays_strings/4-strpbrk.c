3cca7e2d83e325fa31d86fc5c2d306dfdb80ea45
Vee2002 <jackyvee20@gmail.com>
2022-09-26 10:07:59 -0700

    4-strpbrk.c

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -18,9 +18,14 @@ char *_strpbrk(char *s, char *accept)
 		for (j = 0; accept[j]; j++)
 		{
 			if (s[i] == accept[j])
-				break;
+			{
+				return (s + i);
+			}
+			else
+			{
+				return ('\0');
+			}
 		}
-			if (accept[j])
-				return(s + i);
-	}			
+	}
+return (0);	
 }

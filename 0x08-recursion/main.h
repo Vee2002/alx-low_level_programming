cf5da6a78fb693b0e0746ff05be6fa100b9a8be0
Vee2002 <jackyvee20@gmail.com>
2022-09-28 12:19:37 -0700

    0-puts_recursion.c

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,7 +1,7 @@
-void _puts_recursion(char *s)
-void _print_rev_recursion(char *s)
-strlen_recursion(char *s)
-int factorial(int n)
-pow_recursion(int x, int y)
-sqrt_recursion(int n)
-is_prime_number
+void _puts_recursion(char *s);
+void _print_rev_recursion(char *s);
+strlen_recursion(char *s);
+int factorial(int n);
+pow_recursion(int x, int y);
+int _sqrt_recursion(int n);
+int is_prime_number(int n);

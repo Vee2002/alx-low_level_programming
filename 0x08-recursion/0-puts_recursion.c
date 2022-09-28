1540616c40903fd11b7824366e75efc2a27fd512
Vee2002 <jackyvee20@gmail.com>
2022-09-28 04:41:09 -0700

    0-puts_recursion.c

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+*_puts_recursion - Function used
+*@s: Parameter passed
+*Return: returns nothing
+*/
+
+void _puts_recursion(char *s)
+{
+s[] = "A string"
+return (s);
+}
---
+++ 0x08-recursion/main.h
@@ -0,0 +1,7 @@
+void _puts_recursion(char *s)
+void _print_rev_recursion(char *s)
+strlen_recursion(char *s)
+int factorial(int n)
+pow_recursion(int x, int y)
+sqrt_recursion(int n)
+is_prime_number

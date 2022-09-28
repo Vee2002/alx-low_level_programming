162fee73aaf291fd8de05ab2f8bd24cb172523d4
Vee2002 <jackyvee20@gmail.com>
2022-09-28 14:46:08 -0700

    5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -13,5 +13,22 @@ if (n < 0)
 	return (-1);
 else if (n == 1 || n == 0)
 	return (n);
-return (_sqrt_recursion(n, square));
+return (_sqrt(n, square));
+}
+
+/**
+ * _sqrt - Checks for squareroot
+ * @n: Number to check for squareroot
+ * @i: Squareroot
+ * Return: return 1 if squareroot is found
+ * and -1 if not else recurse
+ */
+
+int _sqrt(int n, int i)
+{
+	if (i * i > n)
+		return (-1);
+	if (i * i == n)
+		return (1);
+	return (_sqrt(n, i + 1));
 }

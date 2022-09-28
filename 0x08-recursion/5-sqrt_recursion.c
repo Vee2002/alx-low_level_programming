4faeb39bf7dc8055f9b6ca2b564d593b58c9cb56
Vee2002 <jackyvee20@gmail.com>
2022-09-28 14:33:41 -0700

    5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -11,7 +11,7 @@ int _sqrt_recursion(int n)
 	int square = 2;
 if (n < 0)
 	return (-1);
-else if (n == 1) || ( n == 0)
+else if (n == 1 || n == 0)
 	return (n);
-return (isqrt(n, square));
+return (_sqrt_recursion(n, square));
 }

e4d304aae2ccd617a37f644f9b4e1a738ef26f53
Vee2002 <jackyvee20@gmail.com>
2022-09-30 14:55:43 -0700

    Multipies two numbers

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -7,24 +7,38 @@
 *Return: returns zero
 */
 
-int main (int argc, char *argv[])
+int main(int argc, char *argv[])
 {
-int i, product = 1;
 
-if (argc > 1 && (argc - 1) == 2)
-{
-for (count = 0; count < argc; count++)
-{
-product *= atoi(argv[count]);
-}
-printf("%d\n",product);
-}
-else
-{
-printf("Error\n");
-return (1);
-}
+	int i, product = 1;
 
-return (0);
+
+	if (argc > 1 && (argc - 1) == 2)
+
+	{
+
+		for (count = 0; count < argc; count++)
+
+		{
+
+			product *= atoi(argv[count]);
+
+		}
+
+		printf("%d\n",product);
+
+	}
+
+	else
+
+	{
+
+		printf("Error\n");
+
+		return (1);
+
+	}
+
+	return (0);
 }
 

c123671c377205971269c200486e2d21d8a9458f
Vee2002 <jackyvee20@gmail.com>
2022-10-06 08:11:06 -0700

    Allocates memory

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -9,11 +9,19 @@
  */
 
 void *malloc_checked(unsigned int b)
-{
-	int *a;
 
-	a = malloc(b);
-	if (a == NULL)
-		exit(98);
-	return (a);
+{
+
+		int *a;
+
+
+
+			a = malloc(b);
+
+				if (a == NULL)
+
+							exit(98);
+
+					return (a);
+
 }

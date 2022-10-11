0fd670b5f739cc273bb8f772e87820fa306c58f6
Vee2002 <jackyvee20@gmail.com>
2022-10-11 13:22:28 -0700

    Variabel of type struct dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -11,9 +11,9 @@
 
 struct dog
 {
-	d->name = name;
-	d->age = age;
-	d->owner = owner;
+	char *name;
+	float age;
+	char *owner;
 };
 typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);

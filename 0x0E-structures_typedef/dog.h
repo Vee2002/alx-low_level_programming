7f6f3bf345504ece824a876f2f6df68f2755cf7f
Vee2002 <jackyvee20@gmail.com>
2022-10-11 13:16:40 -0700

    Variable of type struct dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,5 @@
-#include <stdlib.h>
-#include "dog.h"
+#ifndef DOG_H
+#define DOG_H
 
 /*
 *struct_dog - Variable of type struct dog
@@ -9,12 +9,16 @@
 *d: pointer
 */
 
-void init_dog(struct dog *d, char *name, float age, char *owner)
+struct dog
 {
-	if (dog != NULL)
-	{	
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
-	}
-}
+};
+typedef struct dog dog_t;
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+#endif /*DOG_H*/
